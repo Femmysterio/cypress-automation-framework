ba4da17252d307300f7f07b1be7b87627e991c0a
Femmysterio <140889798+Femmysterio@users.noreply.github.com>
2023-07-30 15:47:09 +0100

    Update inspect-item.js
    Third commit

--- cypress/e2e/integration/automation-test-store/inspect-item.js
+++ cypress/e2e/integration/automation-test-store/inspect-item.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe("Inspect Automation Test Store Items Using Chain Of Commands", () => {
+describe("check Automation Test Store Items Using Chain Of Commands", () => {
     it ("Click on the first item using item header", () => {
         cy.visit('https://automationteststore.com/'); 
         cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname').click();
